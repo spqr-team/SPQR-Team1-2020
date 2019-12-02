e11b061226b5d83fd321db161d5fec53a299167c
u-siri-ous <sanninosiria@gmail.com>
2019-12-02 17:47:03 +0100

    ao

--- src/game.cpp
+++ src/game.cpp
@@ -6,18 +6,22 @@ Game::Game(){
 
 void Game::goalie(){
     compass->readSensor();
+    //drive->prepareDrive(0, 150, 0);
     digitalWrite(LED_G, HIGH);
     if(ball->ballSeen){
-        if(ball->angle >= 0) {
-            drive->prepareDrive(0, 50, 0);
-            drive->drive(0, 50, 0);
+        if(ball->angle >= 0) drive->drive(0, 100, 0);
+        else if(ball->angle <= 45) drive->drive(45, 100, 0);
+    } else {
+        drive->prepareDrive(0, 0, 0);
+        drive->drive(0, 0, 0);
     }
-    } else drive->prepareDrive(0, 0, 0);
 }
 
 void Game::keeper(){
     compass->readSensor();
     digitalWrite(LED_Y, HIGH);
+    drive->prepareDrive(0, 0, 0);
+    drive->drive(0, 0, 0);
 }
 
 
