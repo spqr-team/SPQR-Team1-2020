b3902caf29d8c760fe401ba7902bae12ddb1c5ff
EmaMaker <emamaker0@gmail.com>
2020-01-20 18:41:36 +0100

    conic shaped camera mirror final script

--- utility/OpenMV/conic_eff.py
+++ utility/OpenMV/conic_eff.py
@@ -4,14 +4,30 @@
 
 # Automatic RGB565 Color Tracking Example
 #
+
 import sensor, image, time, pyb, math
 
 from pyb import UART
 uart = UART(3,19200, timeout_char = 1000)
 
+START_BYTE = chr(105) #'i'
+END_BYTE = chr(115) #'s'
+BYTE_UNKNOWN = chr(116) #'t'
+
+y_found = False
+b_found = False
+
+#From Arduino Documentation at: https://www.arduino.cc/reference/en/language/functions/math/map/
+def val_map(x, in_min, in_max, out_min, out_max):
+    x = int(x)
+    in_min = int(in_min)
+    in_max = int(in_max)
+    out_min = int(out_min)
+    out_max = int(out_max)
+    return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
+
 
 # LED Setup ##################################################################
-
 red_led = pyb.LED(1)
 green_led = pyb.LED(2)
 blue_led = pyb.LED(3)
@@ -22,14 +38,8 @@ blue_led.on()
 ##############################################################################
 
 
-#thresholds = [  (30, 100, 15, 127, 15, 127),    # generic_red_thresholds
-#                (30, 100, -64, -8, -32, 32),    # generic_green_thresholds
-#                (0, 15, 0, 40, -80, -20)]       # generic_blue_thresholds
 
-#thresholds = [  (54, 93, -10, 25, 55, 70),    # thresholds yellow goal
-#                (30, 45, 1, 40, -60, -19)]    # thresholds blue goal
-#
-thresholds = [  (55, 98, -14, 12, 7, 55),    # thresholds yellow goal
+thresholds = [  (69, 100, -12, 11, 15, 89),    # thresholds yellow goal
                 (20, 51, -23, 26, -69, -31)]  # thresholds blue goal (6, 31, -15, 4, -35, 0)
 
 roi = (0, 6, 318, 152)
@@ -62,31 +72,28 @@ clock = time.clock()
 ##############################################################################
 
 
-
-# [] list
-# () tupla
-
-'''while(True):
-    clock.tick()
-    img = sensor.snapshot()'''
-
 while(True):
     clock.tick()
 
     blue_led.off()
 
+    y_found = False
+    b_found = False
+
     tt_yellow = [(0,999,0,1)]     ## creo una lista di tuple per il giallo, valore x = 999 : non trovata
     tt_blue = [(0,999,0,2)]       ## creo una lista di tuple per il blue, valore x = 999 : non trovata
 
     img = sensor.snapshot()
-    for blob in img.find_blobs(thresholds, pixels_threshold=75, area_threshold=100, merge = True):
+    for blob in img.find_blobs(thresholds, pixels_threshold=75, area_threshold=115, merge = True):
         img.draw_rectangle(blob.rect())
         img.draw_cross(blob.cx(), blob.cy())
 
         if (blob.code() == 1):
             tt_yellow = tt_yellow +  [ (blob.area(),blob.cx(),blob.cy(),blob.code() ) ]
+            y_found = True
         if (blob.code() == 2):
             tt_blue = tt_blue +  [ (blob.area(),blob.cx(),blob.cy(),blob.code() ) ]
+            b_found = True
 
     tt_yellow.sort(key=lambda tup: tup[0])  ## ordino le liste
     tt_blue.sort(key=lambda tup: tup[0])    ## ordino le liste
@@ -94,24 +101,48 @@ while(True):
     ny = len(tt_yellow)
     nb = len(tt_blue)
 
-    '''Yellow'''
-    area,cx,cy,code = tt_yellow[ny-1]    # coordinata x del piu' grande y se montata al contrario
-    cx = img.width() / 2 - cx
-    cy = img.height() / 2 - cy
-    angle = math.pi/2 - math.atan2(cy, cx)
-    dist = math.sqrt(cx*cx + cy*cy)
-    string_yellow = "Y"+str(cx)+" | "+str(cy)+" | "+str(angle)+" | "+str(dist)+"y"
-    print (string_yellow)   # test on serial terminal
+    y_area, y1_cx, y1_cy, y_code = tt_yellow[ny-1]
+    b_area, b1_cx, b1_cy, b_code = tt_blue[nb-1]
 
-    '''Blue'''
-    area,cx,cy,code = tt_blue[nb-1]    # coordinata x del piu' grande y se montata al contrario
-    cx = img.width() / 2 - cx
-    cy = img.height() / 2 - cy
-    angle = math.pi/2 - math.atan2(cy, cx)
-    dist = math.sqrt(cx*cx + cy*cy)
-    string_blue = "B"+str(cx)+" | "+str(cy)+" | |"+str(angle)+" | "+str(dist)+"b"
-    print (string_blue)   # test on serial terminal
+    y_cx = int(img.width() / 2 - y1_cx)
+    y_cy = int(img.height() / 2 - y1_cy)
+    b_cx = int(img.width() / 2 - b1_cx)
+    b_cy = int(img.height() / 2 - b1_cy)
 
-    #print ("..................................")
+    #Normalize data between 0 and 100
+    if y_found == True:
+        y_cx = val_map(y_cx, -img.width() / 2, img.width() / 2, 0, 100)
+        y_cy = val_map(y_cy, -img.height() / 2, img.height() / 2, 0, 100)
+        #Prepare for send as a list of characters
+        s_ycx = chr(y_cx)
+        s_ycy = chr(y_cy)
+    else:
+        y_cx = BYTE_UNKNOWN
+        y_cy = BYTE_UNKNOWN
+        #Prepare for send as a list of characters
+        s_ycx = y_cx
+        s_ycy = y_cy
 
-print(clock.fps())
+    if b_found == True:
+        b_cx = val_map(b_cx, -img.width() / 2, img.width() / 2, 0, 100)
+        b_cx = val_map(b_cy, -img.height() / 2, img.height() / 2, 0, 100)
+
+        #Prepare for send as a list of characters
+        s_bcx = chr(b_cx)
+        s_bcy = chr(b_cy)
+    else:
+        b_cx = BYTE_UNKNOWN
+        b_cy = BYTE_UNKNOWN
+        #Prepare for send as a list of characters
+        s_bcx = b_cx
+        s_bcy = b_cy
+
+    #print(str(y_cx) + " | " + str(y_cy) + "  ---  " + str(b_cx) + " | " + str(b_cy))
+
+    uart.write(START_BYTE)
+    uart.write(s_ycx)
+    uart.write(s_ycy)
+    uart.write(s_bcx)
+    uart.write(s_bcy)
+    uart.write(END_BYTE)
+
